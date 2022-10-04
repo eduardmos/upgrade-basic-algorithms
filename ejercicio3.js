f10bea8f5bd0e6e9cd6078c544a030b0d1d357d7
edward <edumarim@hotmail.com>
2022-10-04 23:09:24 +0200

    firstexercisesjs

---
+++ ejercicio3.js
@@ -0,0 +1,20 @@
+//1.1 Multiplica 10 por 5 y muestra el resultado mediante alert.
+var multiplcacion = 10*5
+alert(multiplicacion)
+
+//1.2 Divide 10 por 2 y muestra el resultado en un alert.
+var division = 10/2
+alert( division )
+//1.3 Muestra mediante un alert el resto de dividir 15 por 9.
+
+//1.4 Usa el correcto operador de asignación que resultará en x = 15, 
+//teniendo dos variables y = 10 y z = 5.
+var y = 10
+var z = 5
+var x = y + z
+
+//1.5 Usa el correcto operador de asignación que resultará en x = 50,
+//teniendo dos variables y = 10 y z = 5.
+var y = 10
+var z = 5
+var x = y * z
